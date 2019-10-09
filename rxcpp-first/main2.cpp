b6cd7809f7af78782411835d6f29615e69d13e26
curtkim <iamteri@gmail.com>
2019-10-10 00:16:14 +0900

    rxcpp 예제추가

---
+++ rxcpp-first/main2.cpp
@@ -0,0 +1,48 @@
+#include "rx.hpp"
+
+namespace Rx {
+    using namespace rxcpp;
+    using namespace rxcpp::sources;
+    using namespace rxcpp::operators;
+    using namespace rxcpp::util;
+}
+using namespace Rx;
+
+int main() {
+
+    // create
+    auto ints = rxcpp::observable<>::create<int>(
+            [](rxcpp::subscriber<int> s){
+                s.on_next(1);
+                s.on_next(2);
+                s.on_completed();
+            });
+    ints.
+            subscribe(
+            [](int v){printf("OnNext: %d\n", v);},
+            [](){printf("OnCompleted\n");});
+
+    // iterate
+    std::array< int, 3 > a={{1, 2, 3}};
+    auto values1 = observable<>::iterate(a);
+    values1.
+            subscribe(
+            [](int v){printf("OnNext: %d\n", v);},
+            [](){printf("OnCompleted\n");});
+
+    // range
+    auto values = observable<>::range(1); // infinite (until overflow) stream of integers
+
+    auto s1 = values.
+            take(3).
+            map([](int prime) { return std::make_tuple("1:", prime); });
+    auto s2 = values.
+            take(3).
+            map([](int prime) { return std::make_tuple("2:", prime); });
+
+    s1.concat(s2).
+            subscribe(rxcpp::util::apply_to(
+            [](const char *s, int p) {
+                printf("%s %d\n", s, p);
+            }));
+}
