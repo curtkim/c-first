1176a73c59b8de09a2a36eec656303a05e41237e
curt.k <curt.k@kakaocorp.com>
2019-11-12 16:27:56 +0900

    modern-cpp14에서 shared_ptr예제 수정

--- modern-cpp14/test_shared_ptr.cpp
+++ modern-cpp14/test_shared_ptr.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <memory>
-#include <typeinfo>
 
-#include <iterator>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -46,44 +43,10 @@ void typeinfo()
   std::cout << "typeid" <<  typeid(b).name() << '\n';
 }
 
-void vector_iterate() {
-  cout << "=== " << __FUNCTION__ << endl;
-  vector<int> ar = { 1, 2, 3, 4, 5 };
-
-  // Declaring iterator to a vector
-  vector<int>::iterator ptr;
-
-  // Displaying vector elements using begin() and end()
-  cout << "The vector elements are : ";
-  for (ptr = ar.begin(); ptr < ar.end(); ptr++)
-    cout << *ptr << " ";
-  cout << endl;
-}
-
-void vector_contains() {
-  cout << "=== " << __FUNCTION__ << endl;
-  vector<string> v = { "1", "2", "3" };
-
-  bool contains = std::find(v.begin(), v.end(), "3") != v.end();
-  cout << (contains ? "contains" : "no contains") << endl;
-}
-
-void vector_distance() {
-  cout << "=== " << __FUNCTION__ << endl;
-  vector<string> v = { "1", "2", "3" };
-
-  std::vector<string>::iterator it = std::find(v.begin(), v.end(), "3");
-  int index = std::distance(v.begin(), it);
-  cout << "index=" << index << endl;
-  cout << v[index] << endl;
-}
-
 int main() {
 
   test_shared_ptr();
   typeinfo();
-  vector_iterate();
-  vector_contains();
-  vector_distance();
+
   return 0;
 }
