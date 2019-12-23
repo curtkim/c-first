6b882e84d60e9d4317a4cf062889f2426d7be2ab
curt.k <curt.k@kakaocorp.com>
2019-12-23 14:49:07 +0900

    rxcpp-first에 async예제추가

---
+++ rxcpp-first/async.cpp
@@ -0,0 +1,38 @@
+#include <rxcpp/rx.hpp>
+
+using namespace std;
+
+// 300ms sleep 하지만, multithread를 사용해서 전체가 300ms 정도에서 끝난다.
+int main() {
+
+  cout << "thread " << this_thread::get_id() << endl;
+  chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+
+  rxcpp::serialize_one_worker s1 = rxcpp::serialize_new_thread();
+  rxcpp::observe_on_one_worker o1 = rxcpp::observe_on_new_thread();
+
+  rxcpp::sources::range(1, 5)
+    .map([o1](int v) {
+      return rxcpp::sources::just(v * 2)
+        .tap([](int v) {
+          cout << v << " " << this_thread::get_id() << endl;
+          this_thread::sleep_for(chrono::milliseconds(300));
+        })
+        .subscribe_on(o1);
+    })
+    .flat_map([](auto observable) { return observable; })
+    .observe_on(s1)
+    .subscribe(
+      [](int v) {
+        cout << v << " " << this_thread::get_id() << endl;
+      },
+      [begin]() {
+        chrono::steady_clock::time_point end = chrono::steady_clock::now();
+        cout << "Time difference = " << chrono::duration_cast<chrono::milliseconds>(end - begin).count()
+                  << "[ms]" << endl;
+      });
+
+  this_thread::sleep_for(chrono::seconds(2));
+
+  return 0;
+}
