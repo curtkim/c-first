a4b54fe44c7c2adf35170253a45917c29b7ff2e7
curt.k <curt.k@kakaocorp.com>
2019-12-24 10:05:02 +0900

    generic예제 추가

---
+++ modern-cpp/generic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <typename T1, typename T2>
+auto compose(T1 a, T2 b) -> decltype(a + b){
+    return a+b;
+}
+
+template <typename T1, typename T2>
+auto compose2(T1 a, T2 b) {
+    return a+b;
+}
+
+using namespace std;
+
+int main() {
+    cout << compose(1, 2) << endl;
+    cout << compose2(1, 2) << endl;
+    return 0;
+}
