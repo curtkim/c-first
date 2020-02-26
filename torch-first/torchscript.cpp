a75a33dcf694af4e9c192e5569a017bc5416aa9e
curt <iamteri@gmail.com>
2020-02-26 10:52:12 +0900

    torchscript 예제추가

---
+++ torch-first/torchscript.cpp
@@ -0,0 +1,27 @@
+#include <torch/script.h> // One-stop header.
+
+#include <iostream>
+#include <memory>
+
+int main(int argc, const char* argv[]) {
+
+    torch::jit::script::Module module;
+    try {
+        // Deserialize the ScriptModule from a file using torch::jit::load().
+        module = torch::jit::load("../../traced_resnet_model.pt");
+    }
+    catch (const c10::Error& e) {
+        std::cerr << e.msg() << std::endl;
+        std::cerr << "error loading the model\n";
+        return -1;
+    }
+
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(torch::ones({1, 3, 224, 224}));
+
+// Execute the model and turn its output into a tensor.
+    at::Tensor output = module.forward(inputs).toTensor();
+    std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+
+    std::cout << "ok\n";
+}
