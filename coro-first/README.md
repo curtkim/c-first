9894c1cb2c95a4981a47102b617be0a570d92fe0
curt <iamteri@gmail.com>
2020-03-07 14:06:28 +0900

    clang을 이용한 coroutine 예제추가

---
+++ coro-first/README.md
@@ -0,0 +1,11 @@
+## Howto
+
+    sudo update-alternatives --install /usr/bin/clang clang /usr/lib/llvm-9.0/bin/clang 190 --force
+    sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/lib/llvm-9.0/bin/clang++ 190 --force
+
+    mkdir build && cd build
+    CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -GNinja ..
+    ninja
+
+    # TODO
+    LD_LIBRARY_PATH=/usr/lib/llvm-9.0/lib ../coroutines 
---
+++ coro-first/main.cpp
@@ -0,0 +1,35 @@
+#include <experimental/coroutine>
+#include "generator.h"
+#include <iostream>
+
+using coro_exp::generator;
+
+static const double demo_ceiling = 10E44;
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wuninitialized"
+#pragma clang diagnostic ignored "-Wfor-loop-analysis"
+generator<double> fibonacci(const double ceiling) {
+  double j = 0;
+  double i = 1;
+  co_yield j;
+  if (ceiling > j) {
+    do {
+      co_yield i;
+      double tmp = i;
+      i += j;
+      j = tmp;
+    } while (i <= ceiling);
+  }
+}
+#pragma clang diagnostic pop
+
+int main() {
+  std::cout << "Example program using C++20 coroutine to implement a Fibonacci Sequence generator" << '\n';
+  auto iter = fibonacci(demo_ceiling);
+  while(iter.next()) {
+    const auto value = iter.getValue();
+    std::cout << value << '\n';
+  }
+}
