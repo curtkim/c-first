07660621feba0ced4ce847a59f81773c467d75e7
curt <iamteri@gmail.com>
2020-04-12 20:37:35 +0900

    cmake 최신버전 tutorial을 적용

--- cmake_tutorial/README.md
+++ cmake_tutorial/README.md
@@ -1,3 +1,5 @@
 ## cmake tutorial
 
+https://cmake.org/cmake/help/latest/guide/tutorial/index.html#cmake-tutorial
+
 https://cmake.org/cmake-tutorial/
--- cmake_tutorial/step3/tutorial.cxx
+++ cmake_tutorial/step3/tutorial.cxx
@@ -12,9 +12,10 @@ int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
-    fprintf(stdout,"%s Version %d.%d\n", argv[0],
+    fprintf(stdout,"%s Version %d.%d.%d\n", argv[0],
             Tutorial_VERSION_MAJOR,
-            Tutorial_VERSION_MINOR);
+            Tutorial_VERSION_MINOR,
+            Tutorial_VERSION_PATCH);
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
