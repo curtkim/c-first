dc8f89c789d150a3582a01868ca1fb6c79280808
curt.k <curt.k@kakaocorp.com>
2020-04-21 13:59:58 +0900

    rxcpp domain예제

--- rxcpp-first/91_omega.cpp
+++ rxcpp-first/91_omega.cpp
@@ -1,4 +1,6 @@
+#include <vector>
 #include <rxcpp/rx.hpp>
+
 
 namespace Rx {
     using namespace rxcpp;
@@ -15,34 +17,147 @@ using namespace std;
 
 
 
-int main() {
-    auto lidar_period = std::chrono::milliseconds(50);
-    auto gps_period = std::chrono::milliseconds(10);
 
-    auto lidar1$ = rxcpp::sources::interval(lidar_period);
-    auto lidar2$ = rxcpp::sources::interval(lidar_period);
-    auto lidar3$ = rxcpp::sources::interval(lidar_period);
-    auto lidar4$ = rxcpp::sources::interval(lidar_period);
-    auto lidar5$ = rxcpp::sources::interval(lidar_period);
+struct Measure {
+    long frame;
+};
 
-    auto gps$ = rxcpp::sources::interval(gps_period);
+struct CarState : Measure {
+};
 
-    printf("main thread %ld\n", std::this_thread::get_id());
+struct LidarMeasure : Measure {
+};
+struct RadarMeasure : Measure  {
+};
+struct GPSMeasure : Measure {
+};
+struct CameraMeasure : Measure  {
+};
 
-    lidar1$
-    .with_latest_from(lidar2$, lidar3$, lidar4$, lidar5$, gps$, gps$.pairwise(), gps$.buffer_with_time(lidar_period))
-    .subscribe([](std::tuple<long, long, long, long, long, long, std::tuple<long, long>, std::vector<long>> v){
-        //C++17 structured binding:
-        auto [ lidar1, lidar2, lidar3, lidar4, lidar5, last_gps, gps_pair, gps_list ] = v;
-        printf("OnNext: %ld %d %d %d %d %d last_gps=%d\n",
-                std::this_thread::get_id(), lidar1, lidar2, lidar3, lidar4, lidar5, last_gps);
+struct Position {
+    float x;
+    float y;
+};
 
-        for( auto i : gps_list ) {
-            std::cout << i << " ";
-        }
-        std::cout << "pair=" << std::get<0>(gps_pair) << " " << std::get<1>(gps_pair);
-        std::cout << std::endl;
+struct Obstacle {
+    int id;
+    Position pos;
+};
+
+struct TrafficLight {
+};
+
+struct Control {
+    float acc;
+    float steering;
+    float brake;
+};
+
+
+class Route {
+};
+class HDMap {
+};
+
+
+Position localize(LidarMeasure& lidar, GPSMeasure& gps){
+    return Position();
+}
+
+vector<Obstacle> perceive_obstacle(LidarMeasure& lidar, RadarMeasure& front_radar, RadarMeasure& rear_radar, vector<CameraMeasure> images, HDMap& hdmap, Position& pos){
+    return vector<Obstacle>();
+}
+
+vector<TrafficLight> perceive_traffic_light(vector<CameraMeasure> images, HDMap& hdmap, Position& pos){
+    return vector<TrafficLight>();
+}
+
+vector<Position> plan(Route& route, HDMap& hdmap, Position& pos, vector<Obstacle> obstacles, vector<TrafficLight> traffic_lights){
+    return std::vector<Position>();
+}
+
+Control control(vector<Position> waypoints, CarState& carState){
+    return Control();
+}
+
+
+observable<vector<GPSMeasure>> last_elements(observable<GPSMeasure> source, int size) {
+    return source.scan(vector<GPSMeasure>{}, [size](vector<GPSMeasure> list, GPSMeasure i) {
+        list.push_back(i);
+        if( list.size() > size)
+            list.erase(list.begin());
+        return list;
     });
+}
+
+void print_list(std::vector<Measure> list) {
+    for( auto i : list ) {
+        std::cout << i.frame << " ";
+    }
+    std::cout << std::endl;
+}
+
+void publish(Control ctrl){
+
+}
+
+int main() {
+    auto lidar_period = chrono::milliseconds(50);
+    auto radar_period = chrono::milliseconds(20);
+    auto gps_period = chrono::milliseconds(10);
+    auto car_state_period = chrono::milliseconds(10);
+    auto camera_period = chrono::milliseconds(30);
+
+    auto lidar1$ = rxcpp::sources::interval(lidar_period).map([](long frame){ return LidarMeasure{frame};});
+    auto lidar2$ = rxcpp::sources::interval(lidar_period).map([](long frame){ return LidarMeasure{frame};});
+    auto lidar3$ = rxcpp::sources::interval(lidar_period).map([](long frame){ return LidarMeasure{frame};});
+    auto lidar4$ = rxcpp::sources::interval(lidar_period).map([](long frame){ return LidarMeasure{frame};});
+    auto lidar5$ = rxcpp::sources::interval(lidar_period).map([](long frame){ return LidarMeasure{frame};});
+
+    auto gps$ = rxcpp::sources::interval(gps_period).map([](long frame){ return GPSMeasure{frame};});
+
+    auto front_radar$ = rxcpp::sources::interval(radar_period).map([](long frame){ return RadarMeasure{frame};});
+    auto rear_radar$ = rxcpp::sources::interval(radar_period).map([](long frame){ return RadarMeasure{frame};});
+
+    auto camera1$ = rxcpp::sources::interval(camera_period).map([](long frame){ return CameraMeasure{frame};});
+    auto camera2$ = rxcpp::sources::interval(camera_period).map([](long frame){ return CameraMeasure{frame};});
+    auto camera3$ = rxcpp::sources::interval(camera_period).map([](long frame){ return CameraMeasure{frame};});
+
+    auto car_state$ = rxcpp::sources::interval(car_state_period).map([](long frame){ return CarState{frame};});
+
+
+    // init
+    HDMap hdmap;
+    Route route;
+
+    auto result$ = lidar1$
+        .with_latest_from(car_state$, last_elements(gps$, 5), lidar2$, lidar3$, lidar4$, lidar5$, front_radar$, rear_radar$, camera1$, camera2$, camera3$)
+        .map( [&hdmap, &route](std::tuple<LidarMeasure, CarState, vector<GPSMeasure>, LidarMeasure, LidarMeasure, LidarMeasure, LidarMeasure, RadarMeasure, RadarMeasure, CameraMeasure, CameraMeasure, CameraMeasure> v){
+            //C++17 structured binding:
+            auto [ lidar1, car_state, gps_list, lidar2, lidar3, lidar4, lidar5, front_radar, rear_radar, camera1, camera2, camera3 ] = v;
+
+            //printf("OnNext: %ld %d %d %d %d %d \n",
+            //       std::this_thread::get_id(), lidar1.frame, lidar2.frame, lidar3.frame, lidar4.frame, lidar5.frame);
+
+            GPSMeasure last_gps = gps_list.back();
+            vector<CameraMeasure> cameras {camera1, camera2, camera3};
+            vector<LidarMeasure> lidars {lidar1, lidar2, lidar3, lidar4, lidar5};
+
+            Position curr_pos = localize(lidar1, last_gps);
+            vector<Obstacle> obstacles = perceive_obstacle(lidar1, front_radar, rear_radar, cameras, hdmap, curr_pos);
+            vector<TrafficLight> traffic_lights = perceive_traffic_light(cameras, hdmap, curr_pos);
+            vector<Position> waypoints = plan(route, hdmap, curr_pos, obstacles, traffic_lights);
+            Control ctrl = control(waypoints, car_state);
+
+            return make_tuple(lidar1.frame, car_state, curr_pos, obstacles, traffic_lights, waypoints, ctrl);
+        });
+
+    result$
+        .subscribe([](std::tuple<long, CarState, Position, vector<Obstacle>, vector<TrafficLight>, vector<Position>, Control> v){
+            auto [ frame, car_state, curr_pos, obstacles, traffic_lights, waypoints, ctrl ] = v;
+            cout << frame << endl;
+            publish(ctrl);
+        });
 
     return 0;
 }
