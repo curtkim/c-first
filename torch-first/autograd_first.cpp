9ae651b479376d892f1c91c611d43fd2b42f6a35
curt <iamteri@gmail.com>
2020-04-26 22:01:37 +0900

    torch-first수정

---
+++ torch-first/autograd_first.cpp
@@ -0,0 +1,23 @@
+#include <torch/script.h> // One-stop header.
+#include <torch/csrc/autograd/variable.h>
+#include <torch/csrc/autograd/function.h>
+
+#include <iostream>
+
+
+int main() {
+  torch::Tensor a = torch::ones({2, 2}, torch::requires_grad());
+  torch::Tensor b = torch::randn({2, 2});
+  auto c = a + b;
+
+  std::cout << a << std::endl;
+  std::cout << b << std::endl;
+  std::cout << c << std::endl;
+  //std::cout << a.grad() << std::endl;
+
+
+  c.backward(); // a.grad() will now hold the gradient of c w.r.t. a
+
+
+  return 0;
+}
--- torch-first/torchscript.cpp
+++ torch-first/torchscript.cpp
@@ -19,7 +19,7 @@ int main(int argc, const char* argv[]) {
     std::vector<torch::jit::IValue> inputs;
     inputs.push_back(torch::ones({1, 3, 224, 224}));
 
-// Execute the model and turn its output into a tensor.
+    // Execute the model and turn its output into a tensor.
     at::Tensor output = module.forward(inputs).toTensor();
     std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
 
