d7fc2421c4179ec30509ad7cd3bbbeb8f6ee2438
curt.k <curt.k@kakaocorp.com>
2020-05-12 17:58:30 +0900

    minor

--- rxcpp-first/11_concurrent.cpp
+++ rxcpp-first/11_concurrent.cpp
@@ -8,23 +8,24 @@ int main() {
   cout << "main thread : " << this_thread::get_id() << endl;
   chrono::steady_clock::time_point start_time = chrono::steady_clock::now();
 
-  //rxcpp::serialize_one_worker s1 = rxcpp::serialize_new_thread();
+  //rxcpp::identity_one_worker i1 = rxcpp::identity_current_thread();
+  rxcpp::serialize_one_worker s1 = rxcpp::serialize_new_thread();
   rxcpp::observe_on_one_worker o1 = rxcpp::observe_on_new_thread();
 
   rxcpp::sources::range(1, 5)
     .map([o1](int v) {
       return rxcpp::sources::just(v)
         .tap([](int v) {
-          cout << v << " " << this_thread::get_id() << endl; // 새로운 thread에서 실행된다.
+          cout << "map " << v << " " << this_thread::get_id() << endl; // 새로운 thread에서 실행된다.
           this_thread::sleep_for(chrono::milliseconds(300));
         })
         .subscribe_on(o1);
     })
     .flat_map([](auto observable) { return observable; })
-    .observe_on(o1)
+    .observe_on(s1)
     .subscribe(
       [](int v) {
-        cout << v << " " << this_thread::get_id() << endl;
+        cout << "sub " << v << " " << this_thread::get_id() << endl;
       },
       [start_time]() {
         long diff = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start_time).count();
