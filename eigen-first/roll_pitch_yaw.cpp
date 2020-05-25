711165ace2bc381e9f44082806c486f0cfd9dba8
curt.k <curt.k@kakaocorp.com>
2020-05-25 13:35:33 +0900

    minor

--- eigen-first/roll_pitch_yaw.cpp
+++ eigen-first/roll_pitch_yaw.cpp
@@ -4,11 +4,13 @@
 #include <Eigen/Geometry>
 
 Eigen::Quaterniond euler2Quaternion(const double roll, const double pitch, const double yaw ){
-  Eigen::AngleAxisd rollAngle(roll, Eigen::Vector3d::UnitX());
-  Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
-  Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
+  using namespace Eigen;
 
-  Eigen::Quaterniond q = yawAngle * pitchAngle * rollAngle;
+  AngleAxisd rollAngle(roll, Vector3d::UnitX());
+  AngleAxisd pitchAngle(pitch, Vector3d::UnitY());
+  AngleAxisd yawAngle(yaw, Vector3d::UnitZ());
+
+  Quaterniond q = yawAngle * pitchAngle * rollAngle;
   return q;
 }
 
@@ -18,19 +20,21 @@ void assertEquals(Eigen::Vector3d a, Eigen::Vector3d b){
 
 int main() {
 
+  using namespace Eigen;
+
   // 1. euler -> quanterion -> rotate
-  Eigen::Quaterniond quaterniond = euler2Quaternion(0, 0, M_PI/2);
+  Quaterniond quaterniond = euler2Quaternion(0, 0, M_PI/2);
   std::cout << quaterniond.matrix() << std::endl;
   std::cout << "(" << quaterniond.matrix().rows() << "," << quaterniond.matrix().cols() << ")" << std::endl;
 
   std::cout << "==================" << std::endl;
-  Eigen::Vector3d v(1,0,0);
-  assertEquals(Eigen::Vector3d(0,1,0), quaterniond.matrix() * v);
+  Vector3d v(1,0,0);
+  assertEquals(Vector3d(0,1,0), quaterniond.matrix() * v);
 
   // 2. quanterion -> euler
   auto euler = quaterniond.toRotationMatrix().eulerAngles(0, 1, 2);
   std::cout << "Euler from quaternion in roll, pitch, yaw"<< std::endl << euler << std::endl;
-  assertEquals(Eigen::Vector3d(0,0,M_PI/2), euler);
+  assertEquals(Vector3d(0,0,M_PI/2), euler);
 
   return 0;
 }
