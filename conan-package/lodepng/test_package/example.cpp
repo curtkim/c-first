4571860484314e0f9b98df853884239d472ef1f3
curt.k <curt.k@kakaocorp.com>
2020-06-08 14:38:23 +0900

    lodepng test example.cpp수정

--- conan-package/lodepng/test_package/example.cpp
+++ conan-package/lodepng/test_package/example.cpp
@@ -116,6 +116,7 @@ int main()
     std::vector<uint8_t> decoded;
     unsigned width = 0, height = 0;
     unsigned error = lodepng::decode(decoded, width, height, PNG_DATA);
+    std::cout << width << "," << height << std::endl;
     assert(static_cast<bool>(error) == false);
     return 0;
 }
