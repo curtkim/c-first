b1d8d33d2cfa92eaedfc8fa9f5fb4c9b33f3e29e
curt.k <curt.k@kakaocorp.com>
2020-06-12 17:10:29 +0900

    torch named tensor

--- torch-first/01_aten_first.cpp
+++ torch-first/01_aten_first.cpp
@@ -3,10 +3,20 @@
 
 using namespace std;
 
+const c10::ArrayRef RC{
+  at::Dimname::fromSymbol(c10::Symbol::dimname("R")),
+  at::Dimname::fromSymbol(c10::Symbol::dimname("C"))
+};
+
+
 int main() {
-  at::Tensor a = at::ones({2, 2}, at::kInt);
+  at::Tensor a = at::ones({2, 2}, RC, at::kInt);
+  cout << a.get_named_tensor_meta()->names() << endl;
+
   at::Tensor b = at::randn({2, 2}); // float type
   auto c = a + b.to(at::kInt);
+
+  cout << "c.has_names(): " << c.has_names() << endl;
 
   cout << a << endl;
   cout << b << endl;
