e656209228c9e1df0b97832dde4c852e415ccc51
curt.k <curt.k@kakaocorp.com>
2020-06-22 11:51:56 +0900

    datetime수정

--- datetime/time_point.cpp
+++ datetime/time_point.cpp
@@ -5,22 +5,24 @@
 
 int main()
 {
-  std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-  std::chrono::duration<double> d = std::chrono::duration<double>(now.time_since_epoch());
+  using namespace std::chrono;
+
+  system_clock::time_point now = system_clock::now();
+  duration<double> d = duration<double>(now.time_since_epoch());
 
   std::cout << std::setprecision (std::numeric_limits<double>::digits10 + 1)
     << d.count() << std::endl;
 
 
-  std::time_t t_c = std::chrono::system_clock::to_time_t(now - std::chrono::hours(24));
+  std::time_t t_c = system_clock::to_time_t(now - hours(24));
   std::cout << "24 hours ago, the time was "
             << std::put_time(std::localtime(&t_c), "%F %T") << '\n';
 
-  std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+  steady_clock::time_point start = steady_clock::now();
   std::cout << "Hello World\n";
-  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  steady_clock::time_point end = steady_clock::now();
   std::cout << "Printing took "
-            << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+            << duration_cast<std::chrono::microseconds>(end - start).count()
             << "us.\n";
 
   std::cout << "sizeof(start)=" << sizeof(start) << std::endl;
