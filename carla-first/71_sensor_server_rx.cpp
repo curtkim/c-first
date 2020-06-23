0a372c55294459cfc3a9b5cbbd2db281049e78dd
curt.k <curt.k@kakaocorp.com>
2020-06-23 17:06:27 +0900

    carla-first/71_sensor_server_rx.cpp camera 9대 적용

--- carla-first/71_sensor_server_rx.cpp
+++ carla-first/71_sensor_server_rx.cpp
@@ -38,8 +38,8 @@ void do_accept(tcp::acceptor &acceptor) {
   });
 }
 
-auto make_camera_subscriber(std::string&& topic_name) {
-  return [topic_name = std::move(topic_name)](boost::shared_ptr<csd::Image> image){
+auto make_camera_subscriber(std::string&& topic_name, std::ofstream& myfile) {
+  return [topic_name = std::move(topic_name), &myfile](boost::shared_ptr<csd::Image> image){
     std::cout << std::this_thread::get_id() << " " << topic_name << " " << getEpochMillisecond() << " frame=" << image->GetFrame() << " " << image->size() << std::endl;
 
     Header header;
@@ -59,23 +59,28 @@ auto make_camera_subscriber(std::string&& topic_name) {
     buffers.push_back(asio::buffer(topic_name, header.topic_name_length));
     buffers.push_back(asio::buffer(image->data(), header.body_length));
 
-    //myfile.write( (char*)image->data(), header.body_length );
-    //myfile.flush();
+    myfile.write( (char*)image->data(), header.body_length );
+    myfile.flush();
 
     if( _socket ) {
       size_t frame = image->GetFrame();
+      std::size_t length = asio::write(*_socket.get(), buffers);
+      std::cout << std::this_thread::get_id() << " " << topic_name << " " << getEpochMillisecond() << " frame=" << frame
+                << " write len=" << length << " " << header.timepoint - image->GetTimestamp() << std::endl;
+      /*
       asio::async_write(*_socket.get(), buffers, [frame, &topic_name](std::error_code ec, std::size_t length) {
         if (!ec) {
           std::cout << std::this_thread::get_id() << " " << topic_name << " " << getEpochMillisecond() << " frame=" << frame
                     << " write len=" << length << std::endl;
         }
       });
+      */
     }
   };
 }
 
-auto make_lidar_subscriber(std::string&& topic_name) {
-  return [topic_name = std::move(topic_name)](boost::shared_ptr<csd::LidarMeasurement> lidar_data){
+auto make_lidar_subscriber(std::string&& topic_name, std::ofstream & myfile) {
+  return [topic_name = std::move(topic_name), &myfile](boost::shared_ptr<csd::LidarMeasurement> lidar_data){
     std::cout << std::this_thread::get_id() << " " << topic_name << " " << getEpochMillisecond() << " frame=" << lidar_data->GetFrame() << " " << lidar_data->size() << std::endl;
 
     Header header;
@@ -93,17 +98,22 @@ auto make_lidar_subscriber(std::string&& topic_name) {
     buffers.push_back(asio::buffer(topic_name, header.topic_name_length));
     buffers.push_back(asio::buffer(lidar_data->data(), header.body_length));
 
-    //myfile.write( (char*)lidar_data->data(), header.body_length );
-    //myfile.flush();
+    myfile.write( (char*)lidar_data->data(), header.body_length );
+    myfile.flush();
 
     if( _socket ) {
       size_t frame = lidar_data->GetFrame();
+      std::size_t length = asio::write(*_socket.get(), buffers);
+      std::cout << std::this_thread::get_id() << " " << topic_name << " " << getEpochMillisecond() << " frame=" << frame
+                << " write len=" << length << " " << header.timepoint - lidar_data->GetTimestamp() << std::endl;
+      /*
       asio::async_write(*_socket.get(), buffers, [topic_name, frame](std::error_code ec, std::size_t length) {
         if (!ec) {
           std::cout << std::this_thread::get_id() << " " << topic_name << " " << getEpochMillisecond() << " frame=" << frame
                     << " write len=" << length << std::endl;
         }
       });
+       */
     }
   };
 }
@@ -112,6 +122,7 @@ int main(int argc, const char *argv[]) {
 
   // asio
   asio::io_context io_context;
+  asio::io_context::strand strand(io_context);
 
   tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 7000));
   do_accept(acceptor);
@@ -135,21 +146,63 @@ int main(int argc, const char *argv[]) {
   std::map<std::string, std::string> camera_attributes = {{"sensor_tick", "0.033"}};
   auto [camera, camera$] = from_sensor_data<csd::Image>(
     world, "sensor.camera.rgb", camera_attributes, camera_transform, vehicle);
-  camera$.subscribe(make_camera_subscriber("/camera/0"));
+  camera$.subscribe(make_camera_subscriber("/camera/0", myfile));
 
   auto camera_transform1 = cg::Transform{
     cg::Location{1.0f, 0.0f, 2.8f},   // x, y, z.
     cg::Rotation{0.0f, 0.0f, 0.0f}}; // pitch, yaw, roll.
   auto [camera1, camera1$] = from_sensor_data<csd::Image>(
     world, "sensor.camera.rgb", camera_attributes, camera_transform1, vehicle);
-  camera1$.subscribe(make_camera_subscriber("/camera/1"));
+  camera1$.subscribe(make_camera_subscriber("/camera/1", myfile));
 
   auto camera_transform2 = cg::Transform{
     cg::Location{0.0f, 0.0f, 2.8f},   // x, y, z.
     cg::Rotation{0.0f, -180.0f, 0.0f}}; // pitch, yaw, roll.
   auto [camera2, camera2$] = from_sensor_data<csd::Image>(
     world, "sensor.camera.rgb", camera_attributes, camera_transform2, vehicle);
-  camera2$.subscribe(make_camera_subscriber("/camera/2"));
+  camera2$.subscribe(make_camera_subscriber("/camera/2", myfile));
+
+  auto camera_transform3 = cg::Transform{
+    cg::Location{0.0f, 0.0f, 2.8f},   // x, y, z.
+    cg::Rotation{0.0f, 90.0f, 0.0f}}; // pitch, yaw, roll.
+  auto [camera3, camera3$] = from_sensor_data<csd::Image>(
+    world, "sensor.camera.rgb", camera_attributes, camera_transform3, vehicle);
+  camera3$.subscribe(make_camera_subscriber("/camera/3", myfile));
+
+  auto camera_transform4 = cg::Transform{
+    cg::Location{0.0f, 0.0f, 2.8f},   // x, y, z.
+    cg::Rotation{0.0f, -90.0f, 0.0f}}; // pitch, yaw, roll.
+  auto [camera4, camera4$] = from_sensor_data<csd::Image>(
+    world, "sensor.camera.rgb", camera_attributes, camera_transform4, vehicle);
+  camera4$.subscribe(make_camera_subscriber("/camera/4", myfile));
+
+  auto camera_transform5 = cg::Transform{
+    cg::Location{0.0f, 0.0f, 2.8f},   // x, y, z.
+    cg::Rotation{0.0f, -90.0f, 0.0f}}; // pitch, yaw, roll.
+  auto [camera5, camera5$] = from_sensor_data<csd::Image>(
+    world, "sensor.camera.rgb", camera_attributes, camera_transform5, vehicle);
+  camera5$.subscribe(make_camera_subscriber("/camera/5", myfile));
+
+  auto camera_transform6 = cg::Transform{
+    cg::Location{0.0f, 0.0f, 2.8f},   // x, y, z.
+    cg::Rotation{0.0f, -90.0f, 0.0f}}; // pitch, yaw, roll.
+  auto [camera6, camera6$] = from_sensor_data<csd::Image>(
+    world, "sensor.camera.rgb", camera_attributes, camera_transform6, vehicle);
+  camera6$.subscribe(make_camera_subscriber("/camera/6", myfile));
+
+  auto camera_transform7 = cg::Transform{
+    cg::Location{0.0f, 0.0f, 2.8f},   // x, y, z.
+    cg::Rotation{0.0f, -90.0f, 0.0f}}; // pitch, yaw, roll.
+  auto [camera7, camera7$] = from_sensor_data<csd::Image>(
+    world, "sensor.camera.rgb", camera_attributes, camera_transform7, vehicle);
+  camera7$.subscribe(make_camera_subscriber("/camera/7", myfile));
+
+  auto camera_transform8 = cg::Transform{
+    cg::Location{0.0f, 0.0f, 2.8f},   // x, y, z.
+    cg::Rotation{0.0f, -90.0f, 0.0f}}; // pitch, yaw, roll.
+  auto [camera8, camera8$] = from_sensor_data<csd::Image>(
+    world, "sensor.camera.rgb", camera_attributes, camera_transform8, vehicle);
+  camera8$.subscribe(make_camera_subscriber("/camera/8", myfile));
 
 
   auto lidar_transform = cg::Transform{
@@ -166,7 +219,7 @@ int main(int argc, const char *argv[]) {
   auto [lidar, lidar$] = from_sensor_data<csd::LidarMeasurement>(
     world, "sensor.lidar.ray_cast", lidar_attributes, lidar_transform, vehicle);
 
-  lidar$.subscribe(make_lidar_subscriber("/lidar/0"));
+  lidar$.subscribe(make_lidar_subscriber("/lidar/0", myfile));
 
 
   vehicle->SetAutopilot(true);
@@ -180,11 +233,24 @@ int main(int argc, const char *argv[]) {
   camera->Stop();
   camera1->Stop();
   camera2->Stop();
+  camera3->Stop();
+  camera4->Stop();
+  camera5->Stop();
+  camera6->Stop();
+  camera7->Stop();
+  camera8->Stop();
 
   lidar->Destroy();
   camera->Destroy();
   camera1->Destroy();
   camera2->Destroy();
+  camera3->Destroy();
+  camera4->Destroy();
+  camera5->Destroy();
+  camera6->Destroy();
+  camera7->Destroy();
+  camera8->Destroy();
+
   vehicle->Destroy();
 
   std::cout << std::this_thread::get_id() << " Actors destroyed." << std::endl;
