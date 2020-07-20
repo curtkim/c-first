c1d98777ed948da3a53057e77d434b184844dd79
curt.k <curt.k@kakaocorp.com>
2020-07-20 13:51:43 +0900

    benchmark에 nano_lodepng추가

--- benchmark/nano_first.cpp
+++ benchmark/nano_first.cpp
@@ -1,13 +1,16 @@
 #define ANKERL_NANOBENCH_IMPLEMENT
+
 #include <nanobench.h>
 
 int main() {
   double d = 1.0;
-  ankerl::nanobench::Bench().minEpochIterations(5000).run("some double ops", [&] {
-    d += 1.0 / d;
-    if (d > 5.0) {
-      d -= 5.0;
-    }
-    ankerl::nanobench::doNotOptimizeAway(d);
-  });
+  ankerl::nanobench::Bench().minEpochIterations(5000).run(
+    "some double ops",
+    [&] {
+      d += 1.0 / d;
+      if (d > 5.0) {
+        d -= 5.0;
+      }
+      ankerl::nanobench::doNotOptimizeAway(d);
+    });
 }
