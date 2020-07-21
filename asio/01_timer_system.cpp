98c13b4ed8c01bc2cd89fa041b9490478341e7d6
curt.k <curt.k@kakaocorp.com>
2020-07-21 16:53:29 +0900

    minor

--- asio/01_timer_system.cpp
+++ asio/01_timer_system.cpp
@@ -5,8 +5,7 @@ int main() {
     asio::io_context io_context;
 
     asio::system_timer timer(io_context,std::chrono::system_clock::now() + std::chrono::seconds(2));
-    timer.async_wait(
-        [](const std::error_code & /*error*/) { std::cout << "timeout\n"; });
+    timer.async_wait([](const std::error_code & /*error*/) { std::cout << "timeout\n"; });
 
     std::cout << "---before run" << std::endl;
     io_context.run();
