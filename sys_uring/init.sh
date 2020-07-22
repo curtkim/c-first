231978e9d84dda936e4b296a98b789a1f4a60d7f
curt.k <curt.k@kakaocorp.com>
2020-07-22 16:35:03 +0900

    sys_uring/init.sh수정

--- sys_uring/init.sh
+++ sys_uring/init.sh
@@ -1,5 +1,11 @@
-apt install -y make
-apt install -y gcc
+apt-get update
+apt-get install -y make
+apt-get install -y gcc
+
+#wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
+#chmod +x ubuntu-mainline-kernel.sh
+#ubuntu-mainline-kernel.sh -i 
+
 git clone https://github.com/axboe/liburing.git
 cd liburing
 make install
