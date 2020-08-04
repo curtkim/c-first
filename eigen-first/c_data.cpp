ba8e30ba73ec554d8bc0b584e407266792fc73df
curt.k <curt.k@kakaocorp.com>
2020-08-04 19:04:06 +0900

    eigen수정

---
+++ eigen-first/c_data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+  ArrayXf v = ArrayXf::LinSpaced(11, 0.f, 10.f);
+  cout << "size=" << v.size() << endl;
+  
+  float *vc = v.data();
+  cout << vc[3] << endl;  // 3.0
+
+  return 0;
+}
