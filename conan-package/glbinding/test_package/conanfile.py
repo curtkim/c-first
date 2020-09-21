90135c88653c563a0dd33a7f58ad5702ca21e2ee
curt <curt.k@kakaomobility.com>
2020-09-21 10:32:00 +0900

    conan glbinding을 추가했으나 ld error

---
+++ conan-package/glbinding/test_package/conanfile.py
@@ -0,0 +1,31 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+
+class GlbindingTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    requires = (
+        "glfw/3.3.2@curt/testing",
+        #"snappy/1.1.8",
+    )    
+
+    def build(self):
+        cmake = CMake(self)
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
+        # in "test_package"
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+        self.copy('*.a*', dst='bin', src='lib')
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            os.chdir("bin")
+            self.run(".%sexample" % os.sep)
