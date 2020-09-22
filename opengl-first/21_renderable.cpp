654bbeab53d05e4aa4316a00f856ce17373aef2f
curt <curt.k@kakaomobility.com>
2020-09-22 10:10:21 +0900

    opengl-first수정

--- opengl-first/21_renderable.cpp
+++ opengl-first/21_renderable.cpp
@@ -1,19 +1,25 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
 #include <Eigen/Core>
+
 #include <igl/frustum.h>
 
 #include <chrono>
 #include <string>
 #include <thread>
 #include <iostream>
+#include <variant>
 
 #include "common/shader.hpp"
 #include "common/camera.hpp"
 #include "common/utils.hpp"
 
-#include "10_grid_renderable.hpp"
+
+#include "20_points_renderable.hpp"
+#include "20_grid_renderable.hpp"
+
 
 std::string vertex_shader = R"(
 #version 330 core
@@ -42,58 +48,59 @@ int w = 1024, h = 768;
 
 using namespace std;
 
-void processKeyboardInput(GLFWwindow *window) {
+
+Camera camera(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90, 00);
+
+float lastX = w / 2.0f;
+float lastY = h / 2.0f;
+bool firstMouse = true;
+
+
+void processInput(GLFWwindow *window) {
   float deltaTime = 0.1;
 
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
+
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    camera.ProcessKeyboard(FORWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    camera.ProcessKeyboard(BACKWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    camera.ProcessKeyboard(LEFT, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
-double lastX = w / 2.0f;
-double lastY = h / 2.0f;
-
-bool panning = false;
-
-glm::vec3 camera_pos(0.0f, -5.0f, 2.0f);
-glm::vec3 camera_target(0.0f, 0.0f, 0.0f);
-
-
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-{
-  std::cout << action << " GLFW_PRESS=" << GLFW_PRESS << std::endl;
-
-  glfwGetCursorPos(window, &lastX, &lastY);
-
-  if( button == GLFW_MOUSE_BUTTON_LEFT )
-    if (action == GLFW_PRESS)
-      panning = true;
-    else
-      panning = false;
-}
-
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-  if( panning) {
-    cout << xpos - lastX << " " << ypos - lastY << std::endl;
-    glm::vec3 delta ((lastX- xpos)/250.0, (ypos-lastY)/250.0, 0.0f);
-    camera_pos += delta;
-    camera_target += delta;
+  if (firstMouse)
+  {
     lastX = xpos;
     lastY = ypos;
+    firstMouse = false;
   }
+
+  float xoffset = xpos - lastX;
+  float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+  lastX = xpos;
+  lastY = ypos;
+
+  camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 int main(int argc, char *argv[]) {
 
   // 1. init
   GLFWwindow * window = make_window(w, h);
-  glfwSetCursorPosCallback(window, mouse_callback);
-  glfwSetMouseButtonCallback(window, mouse_button_callback);
+  //glfwSetCursorPosCallback(window, mouse_callback);
 
   // 2. shader
   GLuint prog_id = LoadShadersFromString(vertex_shader, fragment_shader);
 
-  // 3. model
   static const std::vector<float> g_vertex_buffer_data = {
     // bottom
     -2.0f, -2.0f, 0.0f,
@@ -136,37 +143,51 @@ int main(int argc, char *argv[]) {
     4, 8,
   };
 
+  static const std::vector<float> vertices = {
+    -0.7f, -0.7f, 0.0f, // left
+    0.7f, -0.7f, 0.0f, // right
+    0.0f,  0.7f, 0.0f  // top
+  };
+
+
+  std::cout << "sizeof(std::vector<float>): " << sizeof(std::vector<float>) << std::endl;
+  std::cout << "sizeof(std::vector<unsigned int>): " << sizeof(std::vector<unsigned int>) << std::endl;
+  std::cout << "sizeof(GLuint): " << sizeof(GLuint) << std::endl;
+  std::cout << "sizeof(GridRenderable) : " << sizeof(GridRenderable) << std::endl;
+  std::cout << "sizeof(PointsRenderable) : " << sizeof(PointsRenderable) << std::endl;
+  std::cout << "sizeof(std::variant<GridRenderable, PointsRenderable>) : " << sizeof(std::variant<PointsRenderable, GridRenderable>) << std::endl;
+
+  // 3. model
   auto grid = GridRenderable(g_vertex_buffer_data, indices);
   grid.init();
-
+  auto points = PointsRenderable(vertices);
+  points.init();
 
   // 6. projection
   Eigen::Matrix4f proj = Eigen::Matrix4f::Identity();
   float near = 0.01;
   float far = 100;
-  float fov = 35.;
-  float top = tan(fov / 360. * M_PI) * near;
+  float top = tan(35. / 360. * M_PI) * near;
   float right = top * (double)::w / (double)::h;
-  std::cout << "top=" << top << " right=" << right << std::endl;
-
   igl::frustum(-right, right, -top, top, near, far, proj);
   std::cout << proj << std::endl;
 
   while (!glfwWindowShouldClose(window)) {
 
-    processKeyboardInput(window);
+    processInput(window);
 
     double tic = glfwGetTime();
-
     // clear screen and set viewport
     glClearColor(0.0, 0.0, 0.0, 0.);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // 7. model
     Eigen::Affine3f model = Eigen::Affine3f::Identity();
-    //glm::mat4 view = camera.GetViewMatrix();
-    glm::mat4 view = glm::lookAt(camera_pos, camera_target, glm::vec3(0.0f, 1.0f, 0.0f));
-    //std::cout << "view " << glm::to_string(view) << std::endl;
+    //model.translate(Eigen::Vector3f(0, 0, -1.5));
+    //model.rotate(Eigen::AngleAxisf(0.005 * count++, Eigen::Vector3f(0, 1, 0)));
+    //std::cout << model.matrix() << std::endl;
+
+    glm::mat4 view = camera.GetViewMatrix();
 
     // 8. select program and attach uniforms
     glUseProgram(prog_id);
@@ -175,6 +196,7 @@ int main(int argc, char *argv[]) {
     glUniformMatrix4fv(glGetUniformLocation(prog_id, "model"), 1, GL_FALSE, model.matrix().data());
 
     grid.render();
+    points.render();
 
     glfwSwapBuffers(window);
 
