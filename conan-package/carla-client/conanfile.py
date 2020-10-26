e98b0e7112e8897b9e08e2560a73b225cdcc6470
curt.k <curt.k@kakaocorp.com>
2020-10-26 13:04:15 +0900

    conan-package carla-client 0.9.10.1

--- conan-package/carla-client/conanfile.py
+++ conan-package/carla-client/conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools, CMake
 class CarlaClient(ConanFile):
 
     ## for test
-    #version = "0.9.10"
+    version = "0.9.10.1"
 
     name = "carla-client"
     url = ""
