b3621b7b75f5dcb149b6257e3e2d472ab9670e98
curt.k <curt.k@kakaocorp.com>
2020-11-12 15:21:07 +0900

    ws_timer_server에 thread출력

--- simple-websocket-server-first/ws_timer_server.cpp
+++ simple-websocket-server-first/ws_timer_server.cpp
@@ -12,7 +12,7 @@ int main() {
 
   asio::signal_set signals(*io_context, SIGINT, SIGTERM);
   signals.async_wait([&](const std::error_code&, int signal){
-    cout << "end signal=" << signal << endl;
+    cout << this_thread::get_id() << " end signal=" << signal << endl;
     io_context->stop();
   });
 
@@ -26,9 +26,9 @@ int main() {
   echo.on_message = [](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::InMessage> in_message) {
     auto out_message = in_message->string();
 
-    cout << "Server: Message received: \"" << out_message << "\" from " << connection.get() << endl;
+    cout << this_thread::get_id() << " Server: Message received: \"" << out_message << "\" from " << connection.get() << endl;
 
-    cout << "Server: Sending message \"" << out_message << "\" to " << connection.get() << endl;
+    cout << this_thread::get_id() << " Server: Sending message \"" << out_message << "\" to " << connection.get() << endl;
 
     // connection->send is an asynchronous function
     connection->send(out_message, [](const SimpleWeb::error_code &ec) {
@@ -46,25 +46,26 @@ int main() {
   };
 
   echo.on_open = [](shared_ptr<WsServer::Connection> connection) {
-    cout << "Server: Opened connection " << connection.get() << endl;
+    cout << this_thread::get_id() << " Server: Opened connection " << connection.get() << endl;
   };
 
   // See RFC 6455 7.4.1. for status codes
   echo.on_close = [](shared_ptr<WsServer::Connection> connection, int status, const string & /*reason*/) {
-    cout << "Server: Closed connection " << connection.get() << " with status code " << status << endl;
+    cout << this_thread::get_id() << " Server: Closed connection " << connection.get() << " with status code " << status << endl;
   };
 
   long cnt = 0;
   server.start([&io_context, &server, &cnt](unsigned short port){
-    cout << "Server listening on port " << port << endl << endl;
+    cout << this_thread::get_id() << " Server listening on port " << port << endl << endl;
     TimerContext* t = setInterval(*io_context, [&cnt, &server](){
-      cout << cnt++ << endl;
+      cout << this_thread::get_id() << " " << cnt++ << " conn count=" << server.get_connections().size() << endl;
       for(auto &a_connection : server.get_connections()) {
         a_connection->send(std::to_string(cnt));
       }
     }, 1000);
   });
 
+  cout << this_thread::get_id() << " main thread" << endl;
   io_context->run();
   return 0;
 }
