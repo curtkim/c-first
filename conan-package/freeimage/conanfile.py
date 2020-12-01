0013aa69203488e040c950043e738dd66bc966da
curt <curt.k@kakaomobility.com>
2020-12-02 04:03:27 +0900

    freeimage conan package 추가

---
+++ conan-package/freeimage/conanfile.py
@@ -0,0 +1,47 @@
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+
+
+class FreeImage(ConanFile):
+    name = "freeimage"
+    version = "3.18.0"
+    license = "https://gitlab.lrde.epita.fr/olena/freeimage-mirror/blob/master/license-fi.txt"
+    url = "https://gitlab.lrde.epita.fr/olena/conan-freeimage.git"
+    homepage = "https://gitlab.lrde.epita.fr/olena/mirror-freeimage"
+    description = "FreeImage is an Open Source library project for developers who would like to support popular graphics image formats like PNG, BMP, JPEG, TIFF and others as needed by today's multimedia applications."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=False"
+    generators = "cmake"
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
+
+    def source(self):
+        git = tools.Git()
+        git.clone("{}.git".format(self.homepage))
+        git.checkout("{}".format(self.version))
+        # added
+        tools.replace_in_file("Makefile.gnu", "-o root -g root ", "")
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        env_build_vars = autotools.vars
+        env_build_vars['DESTDIR'] = self.package_folder
+        autotools.make(vars=env_build_vars)
+
+    def package(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        env_build_vars = autotools.vars
+        env_build_vars['DESTDIR'] = self.package_folder
+        autotools.install(vars=env_build_vars)
+
+        with tools.environment_append(autotools.vars):
+            self.run(
+                "mv {}/usr/* {}/".format(self.package_folder, self.package_folder))
+            self.run("rm -rf {}/usr".format(self.package_folder))
+
+    def package_info(self):
+        self.cpp_info.includedirs = ["include"]
+        self.cpp_info.libdirs = ["lib"]
+        self.cpp_info.libs = ["freeimage"]
---
+++ conan-package/freeimage/test_package/test_package.cpp
@@ -0,0 +1,9 @@
+#include <FreeImage.h>
+
+#include <iostream>
+
+
+int main()
+{
+  std::cout << "Packaging FreeImage success!" << std::endl;
+}
