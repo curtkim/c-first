b1a49c414f10ab9fa73ff48e70508de0cfb58074
curt <curt.k@kakaomobility.com>
2020-12-20 04:45:17 +0900

    concept 수정

---
+++ concept/11_std_regular.cpp
@@ -0,0 +1,10 @@
+#include <concepts>
+#include <iostream>
+
+template <std::regular T>
+class MyVector{};
+
+int main() {
+  MyVector<int> myVec1;
+  MyVector<int&> myVec2; // reference is not regular
+}
