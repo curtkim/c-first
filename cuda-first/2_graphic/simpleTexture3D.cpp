f237257368115f28417e506eb49a6b5ff3189b21
curt <curt.k@kakaomobility.com>
2020-12-29 11:09:54 +0900

    minor

--- cuda-first/2_graphic/simpleTexture3D.cpp
+++ cuda-first/2_graphic/simpleTexture3D.cpp
@@ -1,17 +1,5 @@
 /*
- * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
-
-/*
     3D texture sample
-
     This sample loads a 3D volume from disk and displays slices through it
     using 3D texture lookups.
 */
