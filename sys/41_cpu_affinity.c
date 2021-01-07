ac0ffc1b985584d10c69144ee8c19e33f581b7c0
curt <curt.k@kakaomobility.com>
2021-01-07 15:34:52 +0900

    sys에 cpu_affinity추가

---
+++ sys/41_cpu_affinity.c
@@ -0,0 +1,38 @@
+// from https://rocksea.tistory.com/245
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+
+#define __USE_GNU
+
+#include <sched.h>
+
+double waste_time(long n) {
+  double res = 0;
+  long i = 0;
+  while (i < n * 200000) {
+    i++;
+    res += sqrt(i);
+  }
+  return res;
+}
+
+// top에서 1을 누르면 cpu core별로 볼 수 있다.
+int main() {
+  unsigned long mask = 1; /* processor 0 */
+
+  /* bind process to processor 0 */
+  if (sched_setaffinity(0, sizeof(mask), (cpu_set_t *) &mask) < 0) {
+    perror("sched_setaffinity");
+  }
+  /* waste some time so the work is visible with "top" */
+  printf("result: %f\n", waste_time(2000));
+
+  mask = 2; /* process switches to processor 1 now */
+  if (sched_setaffinity(0, sizeof(mask), (cpu_set_t *) &mask) < 0) {
+    perror("sched_setaffinity");
+  }
+  /* waste some more time to see the processor switch */
+  printf("result: %f\n", waste_time(2000));
+  return 0;
+}
