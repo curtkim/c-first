e9311170956ecc9eb69298cb476807e29a04ebcc
curt <curt.k@kakaomobility.com>
2021-01-19 11:49:29 +0900

    fmt.print에 한글 테스트

--- fmt-first/01_first.cpp
+++ fmt-first/01_first.cpp
@@ -2,14 +2,21 @@
 #include <fmt/format.h>
 
 int main() {
+  // format
   std::cout << fmt::format("The answer is {}.", 42) << std::endl;
 
+  // stderr
   fmt::print(stderr, "System error code = {}\n", errno);
 
+  // stdout
   fmt::print("Don't {}\n", "panic");
   fmt::print("I'd rather be {1} than {0}.", "right", "happy");
 
   using namespace fmt::literals;
-  fmt::print("Hello, {name}! The answer is {number}. Goodbye, {name}.",
-             "name"_a="World", "number"_a=42);
+  // {name} 2번 나온다.
+  fmt::print("Hello, {name}! The answer is {number}. Goodbye, {name}.\n",
+             "name"_a="World",
+             "number"_a=42);
+
+  fmt::print("한글은 잘 나오나?\n");
 }
