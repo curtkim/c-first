868ae96e47a7d71d48b6e3a812941d67a35553d4
curt <curt.k@kakaomobility.com>
2021-03-16 09:53:16 +0900

    ring-span vector, end를 넘어가는 경우 test

--- ring-span-lite-first/01-basic.cpp
+++ ring-span-lite-first/01-basic.cpp
@@ -1,7 +1,9 @@
 #include "common.h"
 
+// 생성자(begin, end, first, size)
 int main()
 {
+  // array
   double arr[] = { 2.0 , 3.0, 5.0, };
 
   nonstd::ring_span<double> buffer( arr, arr + dim(arr), arr, dim(arr) );
@@ -11,7 +13,19 @@ int main()
   std::cout << "by index" << "\n";
   std::cout << buffer[0] << " " << buffer[1] << " " << buffer[2] << "\n";
 
+  // end를 넘어가는 경우
   nonstd::ring_span<double> buffer2( arr, arr + dim(arr), arr+1, dim(arr) );
   std::cout << buffer2 << "\n";
 
+
+  // vector
+
+  std::vector<double> vec = {2.0, 3.0, 5.0};
+
+  nonstd::ring_span<double> buffer3( vec.data(), vec.data() + vec.size(), vec.data(), vec.size() );
+  std::cout << buffer3 << "\n";
+
+  // end를 넘어, 일부분만
+  nonstd::ring_span<double> buffer4( vec.data(), vec.data() + vec.size(), vec.data()+2, vec.size()-1 );
+  std::cout << buffer4 << "\n";
 }
