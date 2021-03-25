0b7db7ca7406523e0d28ffe4114161716d83e576
curt.k <curt.k@kakaocorp.com>
2021-03-25 11:48:29 +0900

    minor

--- ring-span-lite-first/01-array.cpp
+++ ring-span-lite-first/01-array.cpp
@@ -1,4 +1,11 @@
 #include "common.h"
+#include <assert.h>
+#include <algorithm>
+#include <stdio.h>
+
+static_assert(sizeof(nonstd::ring_span<double>) == 40);
+static_assert(sizeof(nonstd::ring_span<int>) == 40);
+
 
 // 생성자(begin, end, first, size)
 int main()
@@ -6,15 +13,34 @@ int main()
   // array
   double arr[] = { 2.0 , 3.0, 5.0, };
 
-  nonstd::ring_span<double> buffer( arr, arr + dim(arr), arr, dim(arr) );
-  std::cout << buffer << "\n";
-  std::cout << "sizeof(nonstd::ring_span<double>) = " << sizeof(buffer) << "\n";
+  const nonstd::ring_span<double> buffer( arr, arr + dim(arr), arr, dim(arr) );
 
-  std::cout << "by index" << "\n";
-  std::cout << buffer[0] << " " << buffer[1] << " " << buffer[2] << "\n";
+  assert(buffer.size() == 3);
+  assert(buffer.capacity() == 3);
+  assert(buffer[0] == 2.0);  // by index
+  assert(buffer[1] == 3.0);
+  assert(buffer[2] == 5.0);
 
-  // end를 넘어가는 경우
+  auto it = std::find_if(buffer.begin(), buffer.end(), [](auto i){ return i > 4.0;});
+  assert(*it == 5.0);
+
+  // for loop
+  for(const auto& item : buffer){
+    std::cout << item << ", ";
+  }
+  std::cout << "\n";
+
+
+  // 2. end를 넘어가는 경우
   nonstd::ring_span<double> buffer2( arr, arr + dim(arr), arr+1, dim(arr) );
   std::cout << buffer2 << "\n";
+
+  // 3.
+  nonstd::ring_span<double> buffer3( arr, arr + dim(arr), arr+2, 2 );
+  std::cout << buffer3 << "\n";
+  assert(buffer3.size() == 2);
+  assert(buffer3.capacity() == 3);
+
+  printf("%lu %lu", buffer3.begin(), arr);
 
 }
