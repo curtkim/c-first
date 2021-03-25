cce54fd46ff88af48cc03a6e505fc26099ed76c4
curt.k <curt.k@kakaocorp.com>
2021-03-25 14:57:56 +0900

    ring-span push_back into empty span

--- ring-span-lite-first/05_pushback.cpp
+++ ring-span-lite-first/05_pushback.cpp
@@ -1,6 +1,7 @@
 #include "common.h"
 #include <vector>
 #include <array>
+#include <assert.h>
 
 int main()
 {
@@ -34,12 +35,33 @@ int main()
 
     // 1. push_back
     span.push_back(4);
-    std::cout << arr << "\n";
-    std::cout << span << "\n";
+    assert(2 == span[0]);
+    assert(3 == span[1]);
+    assert(4 == span[2]);
 
     // 2. push_back
     span.push_back(5);
-    std::cout << arr << "\n";
-    std::cout << span << "\n";
+    assert(3 == span[0]);
+    assert(4 == span[1]);
+    assert(5 == span[2]);
   }
+
+  {
+    std::cout << "empty array\n";
+    std::array<int,3> arr = {};
+    nonstd::ring_span<int> span(arr.data(), arr.data() + arr.size(), arr.data(), 0);
+
+    assert(span.empty());
+
+    // 1. push_back
+    span.push_back(1);
+    assert(1 == span[0]);
+
+    span.push_back(2);
+    assert(2 == span.size());
+    assert(1 == span[0]);
+    assert(2 == span[1]);
+  }
+
+
 }
