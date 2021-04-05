88041d61b7d68c44cc69cf8b1e56f54c8f8bf55f
curt <curt.k@kakaomobility.com>
2021-04-05 11:26:25 +0900

    conan-package ipopt추가

---
+++ conan-package/ipopt/README.md
@@ -0,0 +1,14 @@
+## note
+테스트에서 header파일을 찾지 못해서 실패함.
+
+## howto
+
+    conan source . --source-folder=tmp/source
+    conan install . --install-folder=tmp/build
+    CONAN_CPU_COUNT=10 conan build . --source-folder=tmp/source --build-folder=tmp/build
+    conan package . --source-folder=tmp/source --build-folder=tmp/build --package-folder=tmp/package
+
+    conan export-pkg . curt/testing --package-folder=tmp/build/package
+    conan test test_package Ipopt/3.12.7@curt/testing
+    conan create . curt/testing
+
---
+++ conan-package/ipopt/conanfile.py
@@ -0,0 +1,66 @@
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+import os
+import shutil
+
+class Ipopt(ConanFile):
+    name = "Ipopt"
+    version = "3.12.7"
+    url = "https://github.com/coin-or/Ipopt"
+    homepage = "https://github.com/coin-or/Ipopt"
+    description = "COIN-OR Interior Point Optimizer"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False], 
+        "fPIC": [True, False]
+    }
+    default_options = "shared=False", "fPIC=False"
+
+    requires = (
+        "CppAD/20200000.3@curt/testing",
+    )
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
+
+    def source(self):
+        self.run("rm -rf ./*")
+        tools.get(**self.conan_data["sources"][self.version])
+        self.run("mv {}-{}/* .".format(self.name, self.version))
+        self.run("chmod +x configure")
+        self.run('find . -name "install-sh" -exec chmod 0755 {} \;')                
+        self.run("chmod +x get.Mumps && ./get.Mumps", cwd='ThirdParty/Mumps')
+
+    def build(self):    
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.libs.append("m")
+        autotools.configure()
+        autotools.make()
+        autotools.install()
+
+    '''
+    def build(self):
+        print(os.getcwd())
+
+        autotools = AutoToolsBuildEnvironment(self)
+        env_build_vars = autotools.vars
+        env_build_vars['DESTDIR'] = self.package_folder
+        autotools.configure()
+        autotools.make(vars=env_build_vars)
+
+    def package(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        env_build_vars = autotools.vars
+        env_build_vars['DESTDIR'] = self.package_folder
+        autotools.install(vars=env_build_vars)
+
+        #with tools.environment_append(autotools.vars):
+        #    self.run(
+        #        "mv {}/usr/* {}/".format(self.package_folder, self.package_folder))
+        #    self.run("rm -rf {}/usr".format(self.package_folder))
+    '''
+
+    def package_info(self):
+        #self.cpp_info.includedirs = ["include"]
+        #self.cpp_info.libdirs = ["lib"]
+        self.cpp_info.libs = ["ipopt", "coinmumps"]
