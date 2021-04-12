02574463aaf7d1c326cddcd7a787ebe80c121019
curt <curt.k@kakaomobility.com>
2021-04-12 13:18:02 +0900

    minor

--- asio/24_eventfd.cpp
+++ asio/24_eventfd.cpp
@@ -9,11 +9,10 @@
 
 uint64_t value;
 
-void read_from_stream(asio::posix::stream_descriptor& stream) {
-  auto buffer = asio::buffer(&value, sizeof(value));
-  async_read(stream, buffer, [&stream](const std::error_code ec, std::size_t) {
-    //std::cout << std::this_thread::get_id() << " read " << value << std::endl;
-    read_from_stream(stream);
+void read_from_stream(asio::posix::stream_descriptor& stream, asio::mutable_buffer& buffer) {
+  async_read(stream, buffer, [&stream, &buffer](const std::error_code ec, std::size_t) {
+    std::cout << std::this_thread::get_id() << " read " << value << std::endl;
+    read_from_stream(stream, buffer);
   });
 }
 
@@ -31,10 +30,11 @@ int main() {
   asio::io_context io_context;
   asio::posix::stream_descriptor stream{io_context, efd};
 
-  read_from_stream(stream);
+  asio::mutable_buffer buffer = asio::buffer(&value, sizeof(value));
+  read_from_stream(stream, buffer);
 
   std::thread thread([&efd, &start](){
-    for(uint64_t i = 1; i < 1000; i++){
+    for(uint64_t i = 1; i < 10; i++){
       //std::cout << i << " write\n";
       int ret = write(efd, &i, sizeof(uint64_t));
       if (ret != 8)
@@ -43,11 +43,16 @@ int main() {
     }
 
     auto diff = std::chrono::steady_clock::now() - start;
-    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(diff).count() << " \n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    std::cout << "elapsed time " << std::chrono::duration_cast<std::chrono::microseconds>(diff).count() << "μs \n";
     std::exit(0);
   });
 
   io_context.run();
+//  while(true){
+//    io_context.run_one();
+//  }
   thread.join();
 
   return 0;
