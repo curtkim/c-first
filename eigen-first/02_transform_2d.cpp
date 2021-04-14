319239090dd62d6e13fb9182bb8be9d790635c48
curt.k <curt.k@kakaocorp.com>
2021-04-14 17:35:58 +0900

    eigen에 2d transform예제 추가

---
+++ eigen-first/02_transform_2d.cpp
@@ -0,0 +1,32 @@
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <iostream>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+  float arrVertices[] = {-1.0, -1.0,
+                         1.0, -1.0,
+                         1.0, 1.0,
+                         -1.0, 1.0};
+
+  // Map
+  MatrixXf mVertices = Map<Matrix<float, 2, 4> >(arrVertices);
+
+  // U = TRSI
+  Transform<float, 2, Affine> t = Transform<float, 2, Affine>::Identity();
+  //t.scale(0.8f);
+  //t.rotate(AngleAxisf(0.25f * M_PI, Vector3f::UnitX()));
+  //t.translate(Vector2f(2, 0));
+  t.rotate(0.25f * M_PI);
+
+  cout << mVertices.rows() << "," << mVertices.cols() << endl;
+  cout << mVertices << endl;
+
+  cout << "------------------" << endl;
+  cout << mVertices.colwise().homogeneous() << endl;
+  cout << "------------------" << endl;
+
+  cout << t * mVertices.colwise().homogeneous() << endl;
+}
