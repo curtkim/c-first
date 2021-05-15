d5dd2e56b3b609ace99b50fb50cd2e2ab8474b3c
curt <curt.k@kakaomobility.com>
2021-05-15 11:30:02 +0900

    add eigen-cuda추가

---
+++ eigen-cuda/02_second.cu
@@ -0,0 +1,38 @@
+// workaround issue between gcc >= 4.7 and cuda 5.5
+#if (defined __GNUC__) && (__GNUC__ > 4 || __GNUC_MINOR__ >= 7)
+#undef _GLIBCXX_ATOMIC_BUILTINS
+#undef _GLIBCXX_USE_INT128
+#endif
+
+#include <Eigen/Dense>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+void ScalePoints(const float scale, thrust::device_vector<Eigen::Vector3f>& points) {
+    using namespace Eigen;
+
+    thrust::for_each(points.begin(), points.end(),
+                     [=] __device__(Eigen::Vector3f &pt) {
+                         pt = pt * scale;
+                     });
+}
+
+int main() {
+    thrust::host_vector<Eigen::Vector3f> host_points(4);
+
+    host_points[0] = Eigen::Vector3f(-1., -1., 0.);
+    host_points[1] = Eigen::Vector3f(-1., 1., 0.);
+    host_points[2] = Eigen::Vector3f(1., 1., 0.);
+    host_points[3] = Eigen::Vector3f(1., -1., 0.);
+
+    thrust::device_vector<Eigen::Vector3f> points = host_points;
+
+    ScalePoints(2.0f, points);
+    host_points = points;
+
+    for(auto pt : host_points){
+        std::cout << pt << "\n";
+    }
+
+    return 0;
+}
